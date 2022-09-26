991ffc01d4db006d95110c30876ef83fd8bff0b0
Mia Fenneki <chopletskan@gmail.com>
2022-09-27 01:20:48 +1000

    Initial

---
+++ README.md
@@ -0,0 +1,1 @@
+A bunch of utility modules I created and have used for some projects.
---
+++ src/Attribute/init.lua
@@ -0,0 +1,12 @@
+-- Attribute
+-- Mia Fenneki
+-- September 27, 2022
+
+--[=[
+    @class Attribute
+    An Attribute is useful for tracking and updating attributes of an instance.
+]=]
+local Attribute = {}
+Attribute.__index = Attribute
+
+return Attribute
